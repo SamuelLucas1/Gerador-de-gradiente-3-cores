d0db0d87a6d428cb8c9749a0a27bd6374bb307b7
Samuel_Mendes <113310693+SamuelLucas1@users.noreply.github.com>
2023-01-01 14:41:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const directions = document.querySelectorAll(".divDirection i");
+const color01 = document.querySelector("#color01");
+const color02 = document.querySelector("#color02");
+const color03 = document.querySelector("#color03");
+const btnGerar = document.querySelector("button");
+const output = document.querySelector("textarea");
+let direction = "to top";
+
+directions.forEach((item) => {
+  item.onclick = () => {
+    document.querySelector(".active").classList.remove("active");
+
+    item.classList.add("active");
+
+    direction = item.getAttribute("data-dir");
+  };
+});
+
+btnGerar.onclick = () => {
+  const gradient = `linear-gradient(${direction}, ${color01.value}, ${color02.value}, ${color03.value})`;
+
+  document.body.style.backgroundImage = gradient;
+  output.value = gradient;
+};
